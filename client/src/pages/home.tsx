06aebf8c446d14db026f023dcc266337e0b292e4
Furosha <levent.kyllian@gmail.com>
2025-05-23 19:34:38 +0200

    bref

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -2,6 +2,89 @@ import React, { useEffect, useState } from 'react';
 
 import './home.css';
 
+const TimeCounter = () => {
+    const [time, setTime] = useState(new Date());
+    const [prevTime, setPrevTime] = useState(new Date());
+    const [changedIndices, setChangedIndices] = useState<number[]>([]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const newTime = new Date();
+            setPrevTime(time);
+            setTime(newTime);
+            
+            // Calculate which digits changed
+            const timeStr = newTime.toLocaleTimeString('en-US', { 
+                hour12: false,
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            });
+            const prevTimeStr = time.toLocaleTimeString('en-US', { 
+                hour12: false,
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            });
+            
+            const changed: number[] = [];
+            for (let i = 0; i < timeStr.length; i++) {
+                if (timeStr[i] !== prevTimeStr[i]) {
+                    changed.push(i);
+                }
+            }
+            
+            setChangedIndices(changed);
+            
+            setTimeout(() => {
+                setChangedIndices([]);
+            }, 500);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [time]);
+
+    const timeStr = time.toLocaleTimeString('en-US', { 
+        hour12: false,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
+    const prevTimeStr = prevTime.toLocaleTimeString('en-US', { 
+        hour12: false,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
+
+    return (
+        <div className="time-counter">
+            <div className="time-display">
+                {timeStr.split('').map((digit, index) => {
+                    const didChange = changedIndices.includes(index);
+                    const prevDigit = prevTimeStr[index];
+                    
+                    return (
+                        <div 
+                            key={`${index}-${time}`} 
+                            className={`digit ${didChange ? 'changed' : ''} ${digit === ':' ? 'colon' : ''}`}
+                        >
+                            {didChange ? (
+                                <>
+                                    <div className="digit-old">{prevDigit}</div>
+                                    <div className="digit-new">{digit}</div>
+                                </>
+                            ) : (
+                                <div className="digit-static">{digit}</div>
+                            )}
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
 const MoneyCounter = () => {
     const [startTime, setStartTime] = useState('09:00');
     const [endTime, setEndTime] = useState('17:00');
@@ -26,6 +109,37 @@ const MoneyCounter = () => {
         return moneyPerSecond;
     };
 
+    // Calculate money earned so far today
+    const calculateMoneyEarnedToday = () => {
+        const now = new Date();
+        const currentHour = now.getHours();
+        const currentMinute = now.getMinutes();
+        const currentSeconds = now.getSeconds();
+        const currentMinutes = currentHour * 60 + currentMinute;
+        
+        const startMinutes = timeToMinutes(startTime);
+        const endMinutes = timeToMinutes(endTime);
+
+        // If we haven't started working yet today
+        if (currentMinutes < startMinutes) {
+            return 0;
+        }
+
+        // If we're past working hours
+        if (currentMinutes >= endMinutes) {
+            const workMinutes = endMinutes - startMinutes;
+            return (dailyRate / workMinutes) * workMinutes;
+        }
+
+        // If we're currently working
+        const minutesWorked = currentMinutes - startMinutes;
+        const secondsWorked = currentSeconds;
+        const totalSecondsWorked = minutesWorked * 60 + secondsWorked;
+        const moneyPerSecond = calculateMoneyPerSecond();
+        
+        return moneyPerSecond * totalSecondsWorked;
+    };
+
     useEffect(() => {
         const interval = setInterval(() => {
             const now = new Date();
@@ -45,29 +159,27 @@ const MoneyCounter = () => {
                 setIsWorking(false);
             }
 
-            if (isWorking) {
-                const moneyPerSecond = calculateMoneyPerSecond();
-                const nextMoney = money + moneyPerSecond;
-                
-                // Calculate which digits will change
-                const nextMoneyStr = nextMoney.toFixed(2).padStart(maxLength, '0');
-                const currentMoneyStr = money.toFixed(2).padStart(maxLength, '0');
-                
-                const changed: number[] = [];
-                for (let i = 0; i < maxLength; i++) {
-                    if (nextMoneyStr[i] !== currentMoneyStr[i]) {
-                        changed.push(i);
-                    }
+            const currentMoney = calculateMoneyEarnedToday();
+            const nextMoney = currentMoney + (isWorking ? calculateMoneyPerSecond() : 0);
+            
+            // Calculate which digits will change
+            const nextMoneyStr = nextMoney.toFixed(2).padStart(maxLength, '0');
+            const currentMoneyStr = money.toFixed(2).padStart(maxLength, '0');
+            
+            const changed: number[] = [];
+            for (let i = 0; i < maxLength; i++) {
+                if (nextMoneyStr[i] !== currentMoneyStr[i]) {
+                    changed.push(i);
                 }
-                
-                setChangedIndices(changed);
-                setPrevMoney(money);
-                setMoney(nextMoney);
-                
-                setTimeout(() => {
-                    setChangedIndices([]);
-                }, 500);
             }
+            
+            setChangedIndices(changed);
+            setPrevMoney(money);
+            setMoney(nextMoney);
+            
+            setTimeout(() => {
+                setChangedIndices([]);
+            }, 500);
         }, 1000);
 
         return () => clearInterval(interval);
@@ -138,6 +250,9 @@ const MoneyCounter = () => {
                     })}
                 </h1>
             </div>
+
+            <TimeCounter />
+            
             <div className="status">
                 {isWorking ? 'Currently Working' : 'Not Working'}
             </div>
